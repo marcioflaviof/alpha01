9bf1d3e3e853ee290667a4617b84e3ad55d3359a
Marcio Flavio <mflaviof1995@gmail.com>
2020-05-07 17:17:56 -0300

    First test of consuming covid api brasil

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "alpha01/server"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Estados struct {
+	UF     string `json:"uf"`
+	Casos  int    `json:"casos"`
+	Mortes int    `json:"mortes"`
+}
+
+type Caso struct {
+	Data    string `json:"data"`
+	Casos   int    `json:"casos"`
+	Mortes  int    `json:"mortes"`
+	Estados []Estados
+}
 
 func main() {
-	server.Start()
+
+	//var cases interface{}
+	var Casos []Caso
+
+	url := "https://covid-api-brasil.herokuapp.com/casos/2020-05-06"
+
+	testClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := testClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cases1 := Casos
+
+	log.Println(string(body))
+
+	jsonErr := json.Unmarshal(body, &cases1)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+	//fmt.Printf("HTTP: %s\n", res.Status)
+	//fmt.Println(cases1)
+
 }
